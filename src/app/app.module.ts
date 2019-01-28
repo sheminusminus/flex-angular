7795e7c3affc93f3c32cdc8a10db8470e687861a
Emily Kolar <emkolardev@gmail.com>
2019-01-27 19:28:33 -0600

    create ag and product components; route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AgGridComponent } from './ag-grid/ag-grid.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+const appRoutes: Routes = [
+  { path: 'grid', component: AgGridComponent },
+  { path: '', component: ProductDetailComponent },
+  { path: '**', component: AppComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgGridComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
